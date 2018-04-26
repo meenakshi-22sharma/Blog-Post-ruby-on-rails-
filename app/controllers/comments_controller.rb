b7f1e611f99d9abf50df245c39eaa1cf3744e1a2
Meenakshi Sharma <mee2208sharma@gmail.com>
2018-04-26 17:49:26 +0530

    User Authentication while deleting comments or post.Post can be veiwed by everyone but can be posted only through authenetication

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+    http_basic_authenticate_with name: "user", password:"user", only: [:destroy]
    def create
       @post = Post.find(params[:post_id])
       @comment = @post.comments.create(comment_params)
@@ -10,7 +11,7 @@ class CommentsController < ApplicationController
       @comment = @post.comments.find(params[:id])
       @comment.destroy
       redirect_to post_path(@post)
-    end  
+    end
     private def  comment_params
        params.require(:comment).permit(:username, :body)
 
